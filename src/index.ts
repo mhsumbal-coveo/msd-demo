fcf9bb315ccbb8314358d5dc0a83d1c732653dc0
Muhammad Hamza Sumbal <mhsumbal@coveo.com>
2024-01-31 11:58:22 -0500

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { waitForAtomic } from "./utils/atomic";
+
+async function main() {
+  await waitForAtomic();
+  const searchInterface: HTMLAtomicSearchInterfaceElement =
+    document.querySelector("atomic-search-interface")!;
+
+  const platformUrl = process.env.PLATFORM_URL!;
+  const organizationId = process.env.ORGANIZATION_ID!;
+  const accessToken = process.env.API_KEY!;
+  await searchInterface.initialize({
+    accessToken,
+    organizationId,
+    platformUrl,
+  });
+
+  searchInterface.executeFirstSearch();
+}
+
+main();
